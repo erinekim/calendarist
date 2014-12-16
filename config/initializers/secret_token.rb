c704b40b968f91156938f01730b49be654c48f11
Erin Kim <erin.e.kim@gmail.com>
2014-12-16 03:15:27 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calendarist::Application.config.secret_key_base = '97404ff73cc8ac718f0b1340804384b6156f6f5335c2af3cc70e9f3acb7111411794f6649c05525b9b62dca254ccf9776a69fae21f13147bf4052d622ceca8e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Calendarist::Application.config.session_store :cookie_store, key: '_calendarist_session'
